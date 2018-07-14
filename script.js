d765e1c3aaae35d6f9a2f897d31bd6378c037553
Jona Reyes <jonamreyes@gmail.com>
2018-07-14 15:17:19 -0400

    modify css

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 var css = document.querySelector('h3');
 var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
-var body = document.getElementById("gradient");
+var card = document.querySelector(".gradient-card");
+var color1_hex = document.querySelector(".color1_hex");
+var color2_hex = document.querySelector(".color2_hex");
 
 function setGradient(){
-  body.style.background = "linear-gradient(to right," + color1.value + ", " + color2.value + ")";
-  css.textContent = body.style.background + ";"
+  card.style.background = "linear-gradient(to right," + color1.value + ", " + color2.value + ")";
+  hex.textContent = "linear-gradient(to right," + color1.value + ", " + color2.value + ")";
+  rgb.textContent = card.style.background + ";"
+
+  color1_hex.innerHTML = color1.value;
+  color2_hex.innerHTML = color2.value;
+  color1_hex.style.color = color1.value;
+  color2_hex.style.color = color2.value;
 }
 
 color1.addEventListener("input", setGradient);
